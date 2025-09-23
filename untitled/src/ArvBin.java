3e728973cc5343558942e2fab0aeae07d02e3d9c
20241bsi0076 <b1ggcontato@gmail.com>
2025-09-23 10:51:17 -0300

    First Commit

---
+++ untitled/src/ArvBin.java
@@ -0,0 +1,42 @@
+import java.util.Comparator;
+
+public class ArvBin  implements lib.IArvoreBinaria {
+    @Override
+    public void adicionar(Object novoValor) {
+    }
+
+    @Override
+    public Object pesquisar(Object valor) {
+        return null;
+    }
+
+    @Override
+    public Object pesquisar(Object valor, Comparator comparador) {
+        return null;
+    }
+
+    @Override
+    public Object remover(Object valor) {
+        return null;
+    }
+
+    @Override
+    public int altura() {
+        return 0;
+    }
+
+    @Override
+    public int quantidadeNos() {
+        return 0;
+    }
+
+    @Override
+    public String caminharEmNivel() {
+        return "";
+    }
+
+    @Override
+    public String caminharEmOrdem() {
+        return "";
+    }
+}
